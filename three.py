8343a7ee6f6f72e5cc9db72319efb822a0db0069
15ma1a0454 <41770024+15ma1a0454@users.noreply.github.com>
2018-08-14 03:20:55 -0400

    Update three.py

--- three.py
+++ three.py
@@ -2,8 +2,8 @@ t=raw_input("")
 b=['a','e','i','o','u','A','E','I','O','U']
 if (t>='a' and t<='z' or t>='A' and t<='Z'):
    if(t in b):
-            print("vowel")
+            print("Vowel")
    else:
-       print("consonant")
+       print("Consonant")
 else:
      print("invalid")
